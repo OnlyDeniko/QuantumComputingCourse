61971e76a4402c91c515f45e7683743351cf19a9
Rethinking-1 <hfchfc001@mail.ru>
2021-10-30 21:31:14 +0300

    initial

---
+++ modules/task1_prime_numbers/Denisov_D/main.cpp
@@ -0,0 +1,123 @@
+//#define DEBUG
+//#define tests - если порядок чисел в файле пронумерован 
+#define ll long long int
+const int max_sign_num = 25;
+static int counter_prime = 0;
+static int counter_composite = 0;
+static int countrer_carmichael = 0;
+#include"Operation.h"
+int main(int argc, char* argv[], char** env[])
+{
+  auto start1 = std::chrono::steady_clock::now();
+  srand(time(NULL));
+  setlocale(LC_ALL, "rus");
+  char* filein = argv[1];
+  char* fileout = argv[2];
+  std::ofstream fout;
+  std::ifstream fin;
+  std::vector<ll>numbers;
+#ifndef DEBUG
+  if (argc < 3)
+  {
+    std::cout << "Not enough parameters\n";
+    return 0;
+  }
+  fin.open(filein);
+  fout.open(fileout);
+  fout.close();
+#else
+  fout.open("output_file.txt");
+  fout.close();
+  fin.open("carmichael1.txt");
+#endif //DEBUG
+  if (!fin.is_open())
+  {
+    std::cout << "filein open error\n";
+    return 0;
+  }
+  while (!fin.eof())
+  {
+    std::string str = "";
+    getline(fin, str);
+    char s[max_sign_num]{};
+#ifdef tests
+    int k = 0;
+  while (str[k] != ' ')
+    k++;
+  for (size_t i = 0; i < str.size()-k; i++)
+  {
+    s[i] = str[i+k+1];
+    s[str.size()-k-1] = '\0';
+  }
+#else
+    for (size_t i = 0; i < str.size(); i++)
+    {
+      s[i] = str[i];
+    }
+    s[str.size()] = '\0';
+#endif // tests
+    numbers.push_back(std::atoll(s));
+  }
+  fin.close();
+
+
+
+  double max = 0;
+  double medium = 0;
+  //////////////////////////////////////////////
+  for (size_t i = 0; i < numbers.size(); i++)
+  {
+#ifndef DEBUG
+    fout.open(fileout, std::ofstream::app);
+#else
+    fout.open("output_file.txt", std::ofstream::app);
+#endif // DEBUG
+    bool Farm = false;
+    std::string str = std::to_string(numbers[i]);
+    auto start = std::chrono::steady_clock::now();
+    if (!Farm_s_test(numbers[i])) {
+      fout << str << " [composite] - тест Ферма,\t";
+    }
+    else
+    {
+      Farm = true;
+     fout << str << " [prime] - тест Ферма, \t";
+    }
+
+    if (!Farm)
+    {
+     fout << str << " [composite] - Рабин-Миллер, ";
+      counter_composite++;
+    }
+    else if (RobinMiller_test(numbers[i]))
+    {
+      fout << str << " [prime] - Рабин-Миллер, ";
+      counter_prime++;
+    }
+    else
+    {
+      fout << str << " [carmichael] - Рабин-Миллер, ";
+      countrer_carmichael++;
+    }
+
+    auto end = std::chrono::steady_clock::now();
+    std::chrono::duration<double> elapsed_seconds = end - start;
+    if (elapsed_seconds.count() > max)
+      max = elapsed_seconds.count();
+    medium += elapsed_seconds.count();
+   // std::cout << elapsed_seconds.count() * 1000 << "ms\n";
+    fout << elapsed_seconds.count() * 1000  << "ms\n";
+    fout.close();
+  }
+  /////////////////////////////////////////////
+  auto end1 = std::chrono::steady_clock::now();
+  std::chrono::duration<double> elapsed_seconds = end1 - start1;
+  std::cout << "Полное время работы программы = " << elapsed_seconds.count() * 1000 << "ms\n";
+  std::cout << "Составных чисел = " <<counter_composite << "\n";
+  std::cout << "Простых чисел = " << counter_prime << "\n";
+  std::cout << "Чисел Кармайкла = " << countrer_carmichael << "\n";
+  std::cout << "Самая долгая проверка = " << max*1000 << "ms\n";
+  std::cout << "Средняя проверка = " << medium/numbers.size()*1000 << "ms\n";
+  //fout.close();
+  return 0;
+}
